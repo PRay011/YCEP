37af73eb840cdb9fed2258e7b56826bd51495870
PRay <2519219483@qq.com>
2023-07-20 12:52:48 +0800

    'init'

---
+++ ycep-vue/src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(VueAxios, axios);
+app.use(router)
+
+app.mount('#app')
---
+++ ycep-vue/src/utils/common.ts
@@ -0,0 +1,5 @@
+const common = {
+ 
+};
+
+export default common;
