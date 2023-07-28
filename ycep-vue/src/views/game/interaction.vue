1ee732521095ebe10022ed466ecac246cc8a19e6
PRay <2519219483@qq.com>
2023-07-28 19:06:27 +0800

    定义界面以及部分api

---
+++ ycep-vue/src/views/game/interaction.vue
@@ -0,0 +1,21 @@
+<template>interaction</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "interaction",
+  data() {
+    return {
+      data: "",
+    };
+  },
+  components: {},
+  mounted() {},
+  methods: {
+    ready() {},
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
