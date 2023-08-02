50768f4c9e3db31f45351a02c76367313c3d4bb5
Raymond <raymondli631@163.com>
2023-08-02 20:54:20 +0800

    添加python服务端，完善spring客户端

---
+++ ycep-python/Server.py
@@ -0,0 +1,74 @@
+import json
+import uuid
+
+import redis as redis
+
+from main import send
+
+
+class RpcHandler:
+    def __init__(self):
+        # redis连接池
+        pool = redis.ConnectionPool(host='localhost', port=6379, db=6, password='123456', decode_responses=True)
+        self.redis = redis.Redis(connection_pool=pool)
+
+    def __del__(self):
+        self.redis.connection_pool.disconnect()
+
+    def newSession(self):
+        """
+        创建一个新的会话，在redis中添加一条记录用户存储message，以便进行多轮会话
+        返回sessionKey
+        """
+        sessionKey = str(uuid.uuid4())
+        messages = []
+        # 创建缓存，有效期2小时
+        self.redis.set(sessionKey, json.dumps(messages), ex=7200)
+        return sessionKey
+
+    def chat(self, content, sessionKey, sessionType):
+        """
+        :param content: 用户消息
+        :param sessionKey: redis-key，用于提取多轮对话记录
+        :param sessionType: 请求类别，如第几章
+        返回AI对话的结果
+        """
+        messages = json.loads(self.redis.get(sessionKey))
+        # 调用API
+        msgs, response = send(messages, content)
+        # 更新缓存，有效期2小时
+        self.redis.set(sessionKey, json.dumps(msgs), ex=7200)
+        return response
+
+    def closeSession(self, sessionKey):
+        """
+        关闭会话，删除reids对应记录,不需要返回
+        """
+        self.redis.delete(sessionKey)
+
+
+# 启动服务
+# if __name__ == '__main__':
+#     handler = RpcHandler()
+#     processor = AIService.Processor(handler)
+#     transport = TSocket.TServerSocket(host='localhost', port=9090)
+#     tfactory = TTransport.TBufferedTransportFactory()
+#     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+#
+#     server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+#     print('Starting the Python server...')
+#     server.serve()
+
+
+# 本地开发测试
+if __name__ == '__main__':
+    handler = RpcHandler()
+    sessionKey = handler.newSession()
+    while True:
+        # 模拟用户输入
+        content = input("user: ")
+        # 分章节
+        sessionType = 0
+        # 调用接口
+        response = handler.chat(content, sessionKey, sessionType)
+        print(f"AI: {response}")
--- ycep-spring/src/main/java/cn/edu/csu/ycepspring/rpc/ThriftClient.java
+++ ycep-spring/src/main/java/cn/edu/csu/ycepspring/rpc/ThriftClient.java
@@ -18,19 +18,58 @@ public class ThriftClient {
     @Value("${thrift.server.port}")
     private int serverPort;
 
-    public String chat(String message) {
+    /**
+     * 创建一个新的对话
+     */
+    public String newSession() {
         try {
             TTransport transport = new TSocket(serverHost, serverPort);
             transport.open();
             TProtocol protocol = new TBinaryProtocol(transport);
             AIService.Client client = new AIService.Client(protocol);
             // 返回结果
-            String result = client.chat(message);
+            String result = client.newSession();
             transport.close();
             return result;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    /**
+     * 多轮对话
+     */
+    public String chat(String content, String sessionKey, int sessionType) {
+        try {
+            TTransport transport = new TSocket(serverHost, serverPort);
+            transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            AIService.Client client = new AIService.Client(protocol);
+            // 返回结果
+            String result = client.chat(content, sessionKey, sessionType);
+            transport.close();
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 关闭会话
+     */
+    public void closeSession(String sessionKey) {
+        try {
+            TTransport transport = new TSocket(serverHost, serverPort);
+            transport.open();
+            TProtocol protocol = new TBinaryProtocol(transport);
+            AIService.Client client = new AIService.Client(protocol);
+            // 远程调用
+            client.closeSession(sessionKey);
+            transport.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
