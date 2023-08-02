b054e215e8b84577c4856bc4c2a2604ccad55a3a
PRay <2519219483@qq.com>
2023-08-03 00:10:35 +0800

    合并

--- ycep-vue/src/views/game/main.vue
+++ ycep-vue/src/views/game/main.vue
@@ -60,8 +60,10 @@
         ></div>
         <div class="chatBox" v-if="isGame">
           <div class="chat-content">{{ data }}</div>
-          <button class="chat-page-previous">上一页</button>
-          <button class="chat-page-next">下一页</button>
+          <button class="chat-page-previous" @click="previousPage()">
+            上一页
+          </button>
+          <button class="chat-page-next" @click="nextPage()">下一页</button>
         </div>
       </div>
       <div class="game-actions">
@@ -87,10 +89,28 @@ export default defineComponent({
   data() {
     return {
       data: "",
+      // 用于存放剧情
+      plot: [
+        {
+          content: "asjhdfuiashdasiofhnasiofjsaofjasi9of",
+        },
+        {
+          content:
+            "南大学教授孙悦展示了一组由孙悦教授和施智祥教授团队复现的常温常压超导全流程，初步结果显示并没有出现超导现象。该实验采用了多种方法，包括合成前驱物、抽真空、烧结等，但结果与韩国团队的研究中有所不同。孙悦教授表示对于韩国新研究的持「怀疑态度」，但他团队的实验结果仍需要更多的验证和研究。",
+        },
+        {
+          content:
+            "腾讯内部正在研发一款名为“M8”的社交APP，由QQ团队打造，定位是基于地图的虚拟社交产品，主攻年轻人社交赛道。据消息人士透露，产品或于8月份对外开启测试。这是腾讯时隔两年，再度研发的一款重量级别的社交产品。（Tech星球）",
+        },
+      ],
+      //当前页数
+      currentPage: 0,
+      pageNumber: 3,
+      //全屏
       isFullscreen: false,
       loading: false,
       background: defaultImage,
-      myCharacter:'',
+      myCharacter: "",
       game: {
         id: 1,
         title: "",
@@ -181,8 +201,7 @@ export default defineComponent({
       setTimeout(() => {
         this.loading = false;
         this.background = backImage1;
-        this.data =
-          "asdsafsafsafasfasfasfafsafasdsafsafsafasfasfasfafsafasdsafsafsafasfasfasfafsafasdsafsafsafasfasfasfafsafasdsafsafsafasfasfasfafsaf";
+        this.data = this.plot[0].content;
         this.isGame = true;
       }, 1000);
       this.isCharacter = false;
@@ -191,6 +210,26 @@ export default defineComponent({
       this.$router.push("/game/room");
     },
     exitGame() {},
+    // 翻页
+    previousPage() {
+      if (this.currentPage) {
+        let page = --this.currentPage;
+        this.data = this.plot[page].content;
+      } else {
+        alert("已经是第一页了");
+      }
+    },
+    nextPage() {
+      console.log("in");
+      let page = this.currentPage;
+      if (page < this.pageNumber) {
+        console.log("inin");
+        page = ++this.currentPage;
+        this.data = this.plot[page].content;
+      } else {
+        alert("已经是最后一页了");
+      }
+    },
   },
 });
 </script>
