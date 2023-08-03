f6aaf1b9ec9902ab3aa1e31c1e3828888f77ea9a
PRay <2519219483@qq.com>
2023-08-03 11:44:13 +0800

    部分接口

--- ycep-vue/src/api/knowledge/index.ts
+++ ycep-vue/src/api/knowledge/index.ts
@@ -8,3 +8,18 @@ export function getNotice() {
         method: 'get',
     })
 }
+
+//获取知识点
+export function getKnowledge(pageNum:any,pageSize:any) {
+    return request({
+        url: `/main/knowledges?currentPage=${pageNum}&pageSize=${pageSize}`,
+        method: 'get',
+    })
+}
+
+export function getGame(pageNum:any,pageSize:any) {
+    return request({
+        url: `/main/games?currentPage=${pageNum}&pageSize=${pageSize}`,
+        method: 'get',
+    })
+}
--- ycep-vue/src/views/knowledge/index.vue
+++ ycep-vue/src/views/knowledge/index.vue
@@ -1,6 +1,6 @@
 <template>
   <!--  顶部导航栏-->
-  <Top/>
+  <Top />
 
   <div class="container">
     <div class="main">
@@ -8,8 +8,8 @@
       <div class="title-block">
         <el-carousel height="300px" :interval="10000">
           <el-carousel-item v-for="item in imageList" :key="item">
-            <img :src="item" alt="轮播图">
-<!--            <h3 class="small">{{ item }}</h3>-->
+            <img :src="item" alt="轮播图" />
+            <!--            <h3 class="small">{{ item }}</h3>-->
           </el-carousel-item>
         </el-carousel>
       </div>
@@ -28,7 +28,7 @@
           <p class="tag-text">支持联机</p>
         </div>
         <div class="tag-item">
-          <el-icon size="40px"><DocumentChecked/></el-icon>
+          <el-icon size="40px"><DocumentChecked /></el-icon>
           <p class="tag-text">巩固练习</p>
         </div>
         <div class="tag-item">
@@ -39,21 +39,22 @@
           <el-icon size="40px"><DataAnalysis /></el-icon>
           <p class="tag-text">数据可视</p>
         </div>
-
       </div>
 
       <div class="body">
-<!--        <div class="left">-->
-<!--        </div>-->
+        <!--        <div class="left">-->
+        <!--        </div>-->
         <div class="right">
           <!--知识点推荐（可左右翻页）-->
           <template class="block">
             <div class="block-title"><p>您可能感兴趣的知识</p></div>
-            <div class="block-title eng"><p>Knowledge That May Interest You</p></div>
+            <div class="block-title eng">
+              <p>Knowledge That May Interest You</p>
+            </div>
             <div class="block-main">
               <div class="item1">
                 <div class="image">
-                  <img src="../../assets/images/宇宙.jpg" alt="知识点图片">
+                  <img src="../../assets/images/宇宙.jpg" alt="知识点图片" />
                 </div>
                 <div class="cover">
                   <p class="p1">趣味物理</p>
@@ -62,7 +63,7 @@
               </div>
               <div class="item1">
                 <div class="image">
-                  <img src="../../assets/images/氧气.jpg" alt="知识点图片">
+                  <img src="../../assets/images/氧气.jpg" alt="知识点图片" />
                 </div>
                 <div class="cover">
                   <p class="p1">微观化学</p>
@@ -71,43 +72,56 @@
               </div>
             </div>
             <template class="block-cards">
-              <div class="card" v-for="item in knowledgeList" @click="klgDetailBtnClick(item.kind)">
+              <div
+                class="card"
+                v-for="item in knowledgeList"
+                @click="klgDetailBtnClick(item.kind)"
+                :key="item.id"
+              >
                 <div class="card-image">
-                  <img :src="item.imgSrc" alt="背景图片">
+                  <img :src="item.imgSrc" alt="背景图片" />
                 </div>
                 <div class="card-details">
                   <p class="text-title">{{ item.title }}</p>
-                  <p class="text-body">{{ item.body }}</p>
+                  <p class="text-body">{{ item.introduction }}</p>
                   <el-tag class="text-author">作者：{{ item.author }}</el-tag>
                 </div>
                 <button class="card-button">编程</button>
               </div>
             </template>
-
           </template>
 
           <!--游戏推荐（可左右翻页）-->
           <template class="block">
-            <div class="block-title" style="margin-top: 50px;"><p>您可能感兴趣的游戏</p></div>
-            <div class="block-title eng"><p>Games That May Interest You</p></div>
+            <div class="block-title" style="margin-top: 50px">
+              <p>您可能感兴趣的游戏</p>
+            </div>
+            <div class="block-title eng">
+              <p>Games That May Interest You</p>
+            </div>
             <div class="block-main">
               <div class="item2">
                 <el-carousel height="200px" :interval="10000">
                   <el-carousel-item v-for="item in imageList2" :key="item">
-                    <img :src="item" alt="轮播图">
-<!--                    <h3 class="small">{{ item }}</h3>-->
+                    <img :src="item" alt="轮播图" />
+                    <!--                    <h3 class="small">{{ item }}</h3>-->
                   </el-carousel-item>
                 </el-carousel>
               </div>
             </div>
             <template class="block-cards">
-              <div class="card" v-for="item in gameList" @click="klgDetailBtnClick(item.id)">
+              <div
+                class="card"
+                v-for="item in gameList"
+                :key="item.id"
+                @click="klgDetailBtnClick(item.id)"
+              >
                 <div class="card-image">
-                  <img :src="item.imgSrc" alt="背景图片">
+                  <img :src="item.imgSrc" alt="背景图片" />
                 </div>
                 <div class="card-details">
                   <p class="text-title">{{ item.title }}</p>
-                  <p class="text-body">{{ item.body }}</p>
+                  <p class="text-body">{{ item.introduction }}</p>
                   <el-tag class="text-author">作者：{{ item.author }}</el-tag>
                 </div>
                 <button class="card-button">{{ item.kindName }}</button>
@@ -121,118 +135,159 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from "vue";
+import { defineComponent } from "vue";
 import Top from "../../components/top.vue";
+import { getKnowledge, getGame } from "../../api/knowledge/index";
 
 export default defineComponent({
   name: "index",
   data() {
     return {
-      data: "",
-      imageList: ['/src/assets/images/demo/item1.jpg','/src/assets/images/demo/item2.jpg','/src/assets/images/demo/item3.jpg'],
-      imageList2: ['/src/assets/images/demo/game1.jpg','/src/assets/images/demo/game2.jpg','/src/assets/images/demo/game3.jpg'],
+      //当前页
+      pageNum: 1,
+      //一页的个数
+      pageSize: 5,
+      imageList: [
+        "/src/assets/images/demo/item1.jpg",
+        "/src/assets/images/demo/item2.jpg",
+        "/src/assets/images/demo/item3.jpg",
+      ],
+      imageList2: [
+        "/src/assets/images/demo/game1.jpg",
+        "/src/assets/images/demo/game2.jpg",
+        "/src/assets/images/demo/game3.jpg",
+      ],
       knowledgeList: [
         {
-          id:1,
-          imgSrc: '/src/assets/images/灯泡.jpg',
-          title: '电路排查',
+          title: "string",
+          id: 0,
+          author: "string",
           kind: 3,
-          body: '家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！',
-          author: '冷月汐'
+          introduction: "string",
+          image: "/src/assets/images/灯泡.jpg",
         },
         {
-          id:2,
-          imgSrc: '/src/assets/images/灯泡.jpg',
-          title: '电路排查',
+          id: 2,
+          imgSrc: "/src/assets/images/灯泡.jpg",
+          title: "电路排查",
           kind: 3,
-          body: '家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！',
-          author: '冷月汐'
+          introduction: "家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！",
+          author: "冷月汐",
         },
         {
-          id:3,
-          imgSrc: '/src/assets/images/灯泡.jpg',
-          title: '电路排查',
+          id: 3,
+          imgSrc: "/src/assets/images/灯泡.jpg",
+          title: "电路排查",
           kind: 3,
-          body: '家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！',
-          author: '冷月汐'
+          introduction: "家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！",
+          author: "冷月汐",
         },
         {
-          id:4,
-          imgSrc: '/src/assets/images/灯泡.jpg',
-          title: '电路排查',
+          id: 4,
+          imgSrc: "/src/assets/images/灯泡.jpg",
+          title: "电路排查",
           kind: 3,
-          body: '家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！',
-          author: '冷月汐'
+          introduction: "家里的灯泡是怎么亮起来的呢？来这里一探究竟吧！",
+          author: "冷月汐",
         },
         {
-          id:5,
-          imgSrc: '/src/assets/images/氧气.jpg',
-          title: '氧气制取',
+          id: 5,
+          imgSrc: "/src/assets/images/氧气.jpg",
+          title: "氧气制取",
           kind: 3,
-          body: '氧气瓶中的氧气是怎么制造出来呢？',
-          author: '冷月汐'
-        }
+          introduction: "氧气瓶中的氧气是怎么制造出来呢？",
+          author: "冷月汐",
+        },
       ],
       gameList: [
         {
-          id:1,
-          imgSrc: '/src/assets/images/demo/game1.jpg',
-          title: '法庭疑案',
+          id: 1,
+          imgSrc: "/src/assets/images/demo/game1.jpg",
+          title: "法庭疑案",
           kind: 2,
-          kindName:'材料',
-          body: '放学回家的小军路上做了一件事，竟然犯了法！法官有点发愁！',
-          author: '机智的皇冠'
+          kindName: "材料",
+          introduction:
+            "放学回家的小军路上做了一件事，竟然犯了法！法官有点发愁！",
+          author: "机智的皇冠",
         },
         {
-          id:2,
-          imgSrc: '/src/assets/images/demo/game2.jpg',
-          title: '电路排查',
+          id: 2,
+          imgSrc: "/src/assets/images/demo/game2.jpg",
+          title: "电路排查",
           kind: 3,
-          kindName:'物理',
-          body: '莉莉在正常工作，办公室的台灯却突然爆炸了！快来帮帮她吧！',
-          author: '看手机东方红'
+          kindName: "物理",
+          introduction:
+            "莉莉在正常工作，办公室的台灯却突然爆炸了！快来帮帮她吧！",
+          author: "看手机东方红",
         },
         {
-          id:3,
-          imgSrc: '/src/assets/images/demo/game3.jpg',
-          title: '三态变化',
+          id: 3,
+          imgSrc: "/src/assets/images/demo/game3.jpg",
+          title: "三态变化",
           kind: 4,
-          kindName:'化学',
-          body: '空气里究竟都有些什么状态的物质呢？小白走在路上发出了这样的疑问。',
-          author: '梵蒂冈'
+          kindName: "化学",
+          introduction:
+            "空气里究竟都有些什么状态的物质呢？小白走在路上发出了这样的疑问。",
+          author: "梵蒂冈",
         },
         {
-          id:4,
-          imgSrc: '/src/assets/images/demo/game1.jpg',
-          title: '法庭疑案',
+          id: 4,
+          imgSrc: "/src/assets/images/demo/game1.jpg",
+          title: "法庭疑案",
           kind: 2,
-          kindName:'材料',
-          body: '放学回家的小军路上做了一件事，竟然犯了法！法官有点发愁！',
-          author: '机智的皇冠'
+          kindName: "材料",
+          introduction:
+            "放学回家的小军路上做了一件事，竟然犯了法！法官有点发愁！",
+          author: "机智的皇冠",
         },
         {
-          id:5,
-          imgSrc: '/src/assets/images/demo/game2.jpg',
-          title: '电路排查',
+          id: 5,
+          imgSrc: "/src/assets/images/demo/game2.jpg",
+          title: "电路排查",
           kind: 3,
-          kindName:'物理',
-          body: '莉莉在正常工作，办公室的台灯却突然爆炸了！快来帮帮她吧！',
-          author: '看手机东方红'
-        }
+          kindName: "物理",
+          introduction:
+            "莉莉在正常工作，办公室的台灯却突然爆炸了！快来帮帮她吧！",
+          author: "看手机东方红",
+        },
       ],
     };
   },
-  components: {Top},
-  mounted() {
-  },
+  components: { Top },
+  mounted() {},
   methods: {
-    ready() {
+    ready() {},
+
+    showKnowledges() {
+      let that = this;
+      getKnowledge(that.pageNum, that.pageSize)
+        .then((res: any) => {
+          console.log(res);
+          that.pageNum++;
+          that.knowledgeList = res.content;
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
+    },
+
+    showGames() {
+      let that = this;
+      getGame(that.pageNum, that.pageSize)
+        .then((res: any) => {
+          console.log(res);
+          that.pageNum++;
+          that.gameList = res.content;
+        })
+        .catch((err: any) => {
+          console.log(err);
+        });
     },
 
     //点击进入知识点详情
     klgDetailBtnClick(id: number) {
-      this.$router.push("/knowledge/detail/" + id)
-    }
+      this.$router.push("/knowledge/detail/" + id);
+    },
   },
 });
 </script>
