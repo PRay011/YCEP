bfb99187ce43386ca65d6b21b88befbb8b8a0013
Raymond <raymondli631@163.com>
2023-08-05 17:32:06 +0800

    更新数据库脚本

--- ycep-spring/sql/ycep.sql
+++ ycep-spring/sql/ycep.sql
@@ -1,50 +1,56 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.42, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: ycep
+-- ------------------------------------------------------
+-- Server version	5.7.42
 
- Source Server         : MySQL
- Source Server Type    : MySQL
- Source Server Version : 50742 (5.7.42)
- Source Host           : localhost:3306
- Source Schema         : ycep
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50742 (5.7.42)
- File Encoding         : 65001
+--
+-- Table structure for table `auth_local`
+--
 
- Date: 03/08/2023 21:44:04
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for auth_local
--- ----------------------------
 DROP TABLE IF EXISTS `auth_local`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_local` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
-  `passwordBody` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   KEY `account` (`account`),
   CONSTRAINT `auth_local_ibfk_1` FOREIGN KEY (`account`) REFERENCES `user` (`account`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of auth_local
--- ----------------------------
-BEGIN;
-INSERT INTO `auth_local` (`id`, `account`, `username`, `passwordBody`) VALUES (1, 1, 'root', '$argon2id$v=19$m=4096,t=3,p=1$d5FLws7CQLfST/5CzkqS9w$bSrNnvhRyB5lT6/tSgVOiJ7uVSRQR1a5xd4qZbrLQEk');
-INSERT INTO `auth_local` (`id`, `account`, `username`, `passwordBody`) VALUES (2, 2, 'abc', '$argon2id$v=19$m=4096,t=3,p=1$gm87yiZJC94Sc7oeVI4YDQ$nDDky3perv9hmq4V13rPJCfgIQ5kNw1esmzPzvzVzkE');
-INSERT INTO `auth_local` (`id`, `account`, `username`, `passwordBody`) VALUES (3, 3, 'pr', '$argon2id$v=19$m=4096,t=3,p=1$NXLw2uEZLSifb8N+BouhCw$5rmu0gS7KL4UCLP5MybHTY+7os2jaHd0iu7AUvbekwI');
-COMMIT;
+--
+-- Dumping data for table `auth_local`
+--
 
--- ----------------------------
--- Table structure for auth_oauth
--- ----------------------------
+LOCK TABLES `auth_local` WRITE;
+/*!40000 ALTER TABLE `auth_local` DISABLE KEYS */;
+INSERT INTO `auth_local` VALUES (1,1,'root','$argon2id$v=19$m=4096,t=3,p=1$d5FLws7CQLfST/5CzkqS9w$bSrNnvhRyB5lT6/tSgVOiJ7uVSRQR1a5xd4qZbrLQEk'),(2,2,'abc','$argon2id$v=19$m=4096,t=3,p=1$gm87yiZJC94Sc7oeVI4YDQ$nDDky3perv9hmq4V13rPJCfgIQ5kNw1esmzPzvzVzkE'),(3,3,'pr','$argon2id$v=19$m=4096,t=3,p=1$/5tXGdtLgaMwSqo7cpkgTw$84Aq9PPxHywD9Ja8VWbmoRkKj7cEYMmqQ2VZyjJVh0s');
+/*!40000 ALTER TABLE `auth_local` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `auth_oauth`
+--
+
 DROP TABLE IF EXISTS `auth_oauth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `auth_oauth` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `account` int(11) NOT NULL,
@@ -56,98 +62,158 @@ CREATE TABLE `auth_oauth` (
   KEY `account` (`account`),
   CONSTRAINT `auth_oauth_ibfk_1` FOREIGN KEY (`account`) REFERENCES `user` (`account`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of auth_oauth
--- ----------------------------
-BEGIN;
-COMMIT;
+--
+-- Dumping data for table `auth_oauth`
+--
 
--- ----------------------------
--- Table structure for category
--- ----------------------------
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+LOCK TABLES `auth_oauth` WRITE;
+/*!40000 ALTER TABLE `auth_oauth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_oauth` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- ----------------------------
--- Records of category
--- ----------------------------
-BEGIN;
-INSERT INTO `category` (`id`, `category_name`) VALUES (1, '编程');
-INSERT INTO `category` (`id`, `category_name`) VALUES (2, '材料');
-INSERT INTO `category` (`id`, `category_name`) VALUES (3, '物理');
-INSERT INTO `category` (`id`, `category_name`) VALUES (4, '化学');
-COMMIT;
+--
+-- Table structure for table `game`
+--
 
--- ----------------------------
--- Table structure for game
--- ----------------------------
 DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `game` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `game_id` int(11) NOT NULL AUTO_INCREMENT,
   `knowledge_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `img_src` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `knowledge_id` (`knowledge_id`),
-  CONSTRAINT `game_ibfk_1` FOREIGN KEY (`knowledge_id`) REFERENCES `knowledge` (`id`) ON UPDATE CASCADE
+  PRIMARY KEY (`game_id`),
+  KEY `game_ibfk_1` (`knowledge_id`),
+  CONSTRAINT `game_ibfk_1` FOREIGN KEY (`knowledge_id`) REFERENCES `knowledge` (`knowledge_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game`
+--
+
+LOCK TABLES `game` WRITE;
+/*!40000 ALTER TABLE `game` DISABLE KEYS */;
+INSERT INTO `game` VALUES (1,1,'1','1','1','1'),(2,1,'2','2','2','2'),(3,2,'3','3','3','3'),(4,2,'4','4','4','4'),(5,2,'5','5','5','5'),(6,3,'6','6','6','6'),(7,3,'7','7','7','7'),(8,3,'8','8','8','8'),(9,3,'9','9','9','9'),(10,3,'0','0','0','0');
+/*!40000 ALTER TABLE `game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `interest`
+--
+
+DROP TABLE IF EXISTS `interest`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interest` (
+  `account` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of game
--- ----------------------------
-BEGIN;
-COMMIT;
+--
+-- Dumping data for table `interest`
+--
 
--- ----------------------------
--- Table structure for item
--- ----------------------------
+LOCK TABLES `interest` WRITE;
+/*!40000 ALTER TABLE `interest` DISABLE KEYS */;
+INSERT INTO `interest` VALUES (3,1),(3,2);
+/*!40000 ALTER TABLE `interest` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `item`
+--
+
 DROP TABLE IF EXISTS `item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `category_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `kind_id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `item_kind_id_fk` (`kind_id`),
+  CONSTRAINT `item_kind_id_fk` FOREIGN KEY (`kind_id`) REFERENCES `kind` (`kind_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `item`
+--
+
+LOCK TABLES `item` WRITE;
+/*!40000 ALTER TABLE `item` DISABLE KEYS */;
+INSERT INTO `item` VALUES (1,3,'电路'),(2,3,'太空'),(3,3,'力'),(4,3,'电磁'),(5,1,'排序算法'),(6,1,'数组'),(7,1,'链表'),(8,1,'对象');
+/*!40000 ALTER TABLE `item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kind`
+--
+
+DROP TABLE IF EXISTS `kind`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `kind` (
+  `kind_id` int(11) NOT NULL AUTO_INCREMENT,
   `kind_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `category_id` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`kind_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of item
--- ----------------------------
-BEGIN;
-COMMIT;
+--
+-- Dumping data for table `kind`
+--
 
--- ----------------------------
--- Table structure for knowledge
--- ----------------------------
+LOCK TABLES `kind` WRITE;
+/*!40000 ALTER TABLE `kind` DISABLE KEYS */;
+INSERT INTO `kind` VALUES (1,'编程'),(2,'材料'),(3,'物理'),(4,'化学');
+/*!40000 ALTER TABLE `kind` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `knowledge`
+--
+
 DROP TABLE IF EXISTS `knowledge`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `knowledge` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `knowledge_id` int(11) NOT NULL AUTO_INCREMENT,
   `item_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `author` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `img_src` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `kind_id` (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`knowledge_id`),
+  KEY `knowledge_item_id_fk` (`item_id`),
+  CONSTRAINT `knowledge_item_id_fk` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of knowledge
--- ----------------------------
-BEGIN;
-COMMIT;
+--
+-- Dumping data for table `knowledge`
+--
 
--- ----------------------------
--- Table structure for notice
--- ----------------------------
+LOCK TABLES `knowledge` WRITE;
+/*!40000 ALTER TABLE `knowledge` DISABLE KEYS */;
+INSERT INTO `knowledge` VALUES (1,1,'1','1','1','1'),(2,1,'2','2','2','2'),(3,1,'3','3','3','3'),(4,1,'4','4','4','4'),(5,1,'5','5','5','5'),(6,2,'6','6','6','6'),(7,2,'7','7','7','7'),(8,2,'8','8','8','8'),(9,2,'9','9','9','9'),(10,3,'0','0','0','0');
+/*!40000 ALTER TABLE `knowledge` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notice`
+--
+
 DROP TABLE IF EXISTS `notice`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `notice` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `content` varchar(255) NOT NULL,
@@ -155,40 +221,49 @@ CREATE TABLE `notice` (
   `is_delete` tinyint(1) NOT NULL COMMENT '0：未删除；1：已删除',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of notice
--- ----------------------------
-BEGIN;
-INSERT INTO `notice` (`id`, `content`, `release_time`, `is_delete`) VALUES (1, '这是一条公告', '2023-08-02 07:58:51', 0);
-INSERT INTO `notice` (`id`, `content`, `release_time`, `is_delete`) VALUES (2, '这是另外一条公告', '2023-08-02 08:01:53', 0);
-INSERT INTO `notice` (`id`, `content`, `release_time`, `is_delete`) VALUES (3, '这又双叒叕是一条公告', '2023-08-02 08:02:47', 0);
-INSERT INTO `notice` (`id`, `content`, `release_time`, `is_delete`) VALUES (4, '这是一条新的公告', '2023-08-02 08:02:57', 1);
-INSERT INTO `notice` (`id`, `content`, `release_time`, `is_delete`) VALUES (5, '公告xxx', '2023-08-02 08:33:52', 0);
-COMMIT;
+--
+-- Dumping data for table `notice`
+--
 
--- ----------------------------
--- Table structure for role
--- ----------------------------
+LOCK TABLES `notice` WRITE;
+/*!40000 ALTER TABLE `notice` DISABLE KEYS */;
+INSERT INTO `notice` VALUES (1,'这是一条公告','2023-08-02 07:58:51',0),(2,'这是另外一条公告','2023-08-02 08:01:53',0),(3,'这又双叒叕是一条公告','2023-08-02 08:02:47',0),(4,'这是一条新的公告','2023-08-02 08:02:57',1),(5,'公告xxx','2023-08-02 08:33:52',0);
+/*!40000 ALTER TABLE `notice` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
 DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `role_id` int(11) NOT NULL AUTO_INCREMENT,
   `role_name` varchar(255) NOT NULL,
   PRIMARY KEY (`role_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of role
--- ----------------------------
-BEGIN;
-INSERT INTO `role` (`role_id`, `role_name`) VALUES (0, 'admin');
-INSERT INTO `role` (`role_id`, `role_name`) VALUES (1, 'user');
-COMMIT;
+--
+-- Dumping data for table `role`
+--
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (0,'admin'),(1,'user');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `account` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NOT NULL,
@@ -201,14 +276,25 @@ CREATE TABLE `user` (
   KEY `role_id` (`role_id`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-BEGIN;
-INSERT INTO `user` (`account`, `role_id`, `phone`, `email`, `sex`, `school`, `description`) VALUES (1, 0, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `user` (`account`, `role_id`, `phone`, `email`, `sex`, `school`, `description`) VALUES (2, 1, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `user` (`account`, `role_id`, `phone`, `email`, `sex`, `school`, `description`) VALUES (3, 1, NULL, NULL, NULL, NULL, NULL);
-COMMIT;
+--
+-- Dumping data for table `user`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,0,NULL,NULL,NULL,NULL,NULL),(2,1,NULL,NULL,NULL,NULL,NULL),(3,1,'12345678901','123456789@test.com','女','解放幼儿园','哈哈哈');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-05 17:30:56
