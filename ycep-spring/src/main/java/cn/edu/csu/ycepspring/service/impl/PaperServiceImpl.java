d7811e77a6bcafb4e105fce364c4d4498dce45dd
Raymond <raymondli631@163.com>
2023-08-31 12:29:08 +0800

    修改历史论文接口

--- ycep-spring/src/main/java/cn/edu/csu/ycepspring/service/impl/PaperServiceImpl.java
+++ ycep-spring/src/main/java/cn/edu/csu/ycepspring/service/impl/PaperServiceImpl.java
@@ -1,8 +1,10 @@
 package cn.edu.csu.ycepspring.service.impl;
 
+import cn.edu.csu.ycepspring.entity.dto.PaperBrief;
 import cn.edu.csu.ycepspring.entity.po.mongo.PaperDocument;
 import cn.edu.csu.ycepspring.service.PaperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -21,8 +23,21 @@ public class PaperServiceImpl implements PaperService {
     }
 
     @Override
-    public List<PaperDocument> getPapers(int account) {
-        Query query = new Query(Criteria.where("account").is(account));
-        return mongoTemplate.find(query, PaperDocument.class, "paper");
+    public Page<PaperBrief> getPaperList(int account, int pageNum, int pageSize) {
+        // 分页
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize, Sort.by(Sort.Order.desc("time")));
+        // 选择
+        Query query = Query.query(Criteria.where("account").is(account)).with(pageable);
+        // 投影
+        query.fields().include("_id", "time", "title", "keywords", "brief");
+
+        List<PaperBrief> results = mongoTemplate.find(query, PaperBrief.class, "paper");
+        long count = mongoTemplate.count(query, PaperBrief.class, "paper");
+        return new PageImpl<>(results, pageable, count);
+    }
+
+    @Override
+    public PaperDocument getPaper(String id) {
+        return mongoTemplate.findById(id, PaperDocument.class, "paper");
     }
 }
