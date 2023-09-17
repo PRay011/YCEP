cdc676c881eda74ab80c027446b9715e4f997f25
PRay <2519219483@qq.com>
2023-09-17 20:04:15 +0800

    移动端界面

---
+++ ycep-vue/src/views/mobile/user/login.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "login",
+  data() {
+    return {
+      data: "",
+    };
+  },
+  components: {},
+  mounted() {},
+  methods: {
+    ready() {},
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/style/css/mobile/login.scss';
+</style>
---
+++ ycep-vue/src/views/mobile/user/selfcenter.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "selfcenter",
+  data() {
+    return {
+      data: "",
+    };
+  },
+  components: {},
+  mounted() {},
+  methods: {
+    ready() {},
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@import '../../../assets/style/css/mobile/selfcenter.scss';
+</style>
